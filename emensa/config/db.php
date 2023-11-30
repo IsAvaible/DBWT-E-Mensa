f17e4fc532a2154aebe51e0788a5f595e8a22b59
sc1103s <simon.conrad@alumni.fh-aachen.de>
2023-11-30 21:11:15 +0100

    M4 Aufgabe 6

---
+++ emensa/config/db.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => 'root',
+    'database' => 'emensawerbeseite',
+    // optionally: set port below if it differs from the default 3306
+    // 'port' => 13306 // !! this is not your webserver port, but the mariadb port
+];
---
+++ emensa/controllers/ExampleController.php
@@ -0,0 +1,19 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../models/kategorie.php');
+
+class ExampleController
+{
+    public function m4_6a_queryparameter(RequestData $rd)
+    {
+        /*
+           Wenn Sie hier landen:
+           bearbeiten Sie diese Action,
+           so dass Sie die Aufgabe löst
+        */
+
+        return view('notimplemented', [
+            'request' => $rd,
+            'url' => 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"
+        ]);
+    }
+}
